0f56fc2f441f28358cd056fe76025517a3300e3a
imkanami <32579467+imkanami@users.noreply.github.com>
2020-03-04 21:48:30 +0100

    initial build

---
+++ install.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+
+# Install apache
+sudo apt-get install apache2
+
+# Add PPA for PHP 7.4
+sudo apt -y install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+
+# Install PHP 7.4
+sudo apt install php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl -y
+
+# Install mysql-server
+sudo apt install mysql-server
+
+# Install phpmyadmin
+sudo apt install phpmyadmin php-mbstring php-gettext
+sudo phpenmod mbstring
+cd /etc/apache2/
+echo 'Include /etc/phpmyadmin/apache.conf' >> apache2.conf
+sudo systemctl restart apache2
